b06b3f68a68a9439700780a843842487c2b13159
mounvip1525 <mounvip1525@gmail.com>
2021-05-27 00:55:21 +0530

    multiple die added

--- Dice/dicer/src/Die.js
+++ Dice/dicer/src/Die.js
@@ -3,6 +3,7 @@ import "./Die.css";
 
 class Die extends Component{
     render(){
+        let arr = [ "one" , "two" , "three" , "four" , "five" , "six" , "one" , "two"]
         return(
             <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling ? "rolling" : ""}`}/>
         )
--- Dice/dicer/src/RollDie.js
+++ Dice/dicer/src/RollDie.js
@@ -10,26 +10,40 @@ export default class RollDie extends Component {
         super(props)
         this.state = {
             die1: "one",
-            die2:"one",
-            isRolling:false
+            die2: "two",
+            die3: "three",
+            die4: "four",
+            die5: "five",
+            die6: 'six',
+            die7: "one",
+            die8: 'one',
+            isRolling:false,
+            s : ["one","two","three","four","five","six","seven","eight"]
         }
         this.roll=this.roll.bind(this);
     }
     roll(){
         this.setState({isRolling:true,
-                       die1:this.props.sides[Math.floor(Math.random() * this.props.sides.length )],
-                       die2:this.props.sides[Math.floor(Math.random() * this.props.sides.length )]
+                       s:[this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )],this.props.sides[Math.floor(Math.random() * this.props.sides.length )]]
                     });
         setTimeout(()=>{
             this.setState({isRolling:false})
         },1000);  
     }
     render() {
+        let num = [1];
+        console.log(this.props.num);
+        let arr = [1] ; 
+        for(var i=1 ; i< this.props.num ; i++) {
+            arr.push(1);
+        }
+        console.log("arr",arr);
         return (
             <div className="RollDie">
                 <div className="dice-container">
-                    <Die face={this.state.die1} rolling={this.state.isRolling} />
-                    <Die face={this.state.die2} rolling={this.state.isRolling} />
+                {arr.map((a,index)=>{
+                    return <Die face={`${this.state.s[index]}`} rolling={this.state.isRolling} key={index} />
+                })}
                 </div>
                 <button onClick={this.roll} disabled={this.state.isRolling}>{this.state.isRolling ? "Rolling..." : "Roll Dice!"}</button>
             </div>
