67e6ef85d1ecd4842cdd90469237b9d60e3a1561
mounvip1525 <mounvip1525@gmail.com>
2021-05-27 10:25:37 +0530

    lottery complete

--- react-mini-projects/src/components/Lottery/Lottery.js
+++ react-mini-projects/src/components/Lottery/Lottery.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import LotteryBall from './LotteryBall';
-import './styles.css';
 
-export default class Lottery extends Component {
+export default class LotteryBox extends Component {
+    static defaultProps = {
+        title: "Your Lottery Box",
+        maxNum:40,
+        numBalls:6
+    }
+    constructor(props){
+        super(props)
+        this.state = {
+            num : Array.from({length:this.props.numBalls})
+        }
+        this.handleClick=this.handleClick.bind(this);
+    }
+    generate(){
+        this.setState(curState=>({
+            num: curState.num.map(n=>(Math.floor(Math.random() * this.props.maxNum))+1)
+            })
+        )
+    }
+    handleClick(){
+        this.generate();
+    }
     render() {
         return (
-            <div>
-                <LotteryBall num={6}/>
-                <LotteryBall num={6}/>
-                <LotteryBall num={6}/>
-                <LotteryBall num={6}/>
+            <div className="Lottery">
+                <h3>{this.props.title}</h3>
+                <div>
+                    {this.state.num.map(n=>{
+                        return <LotteryBall num={n} />
+                    })}
+                </div>
+                <button onClick={this.handleClick}>Generate</button>
             </div>
         )
     }
--- react-mini-projects/src/containers/Lottery.js
+++ react-mini-projects/src/containers/Lottery.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
-import LotteryBall from "../components/Lottery/LotteryBall";
 import Lottery from '../components/Lottery/Lottery';
 
 export default class LotteryBox extends Component {
-    static defaultProps = {
-        title: "Your Lottery Box",
-        maxNum:40,
-        numBalls:6
-    }
-    constructor(props){
-        super(props)
-        this.state = {
-            num : []
-        }
-        this.handleClick=this.handleClick.bind(this);
-    }
-    generate(){
-
-    }
-    handleClick(){
-        this.generate();
-    }
     render() {
         return (
             <div>
-                <h2>{this.props.title}</h2>
+                <h2>Lottery Generator</h2>
                 <Lottery />
-                <button onClick={this.handleClick}>Generate</button>
+                <Lottery title='Mini Lottery Box' numBalls={4} maxNum={10} />
             </div>
         )
     }
