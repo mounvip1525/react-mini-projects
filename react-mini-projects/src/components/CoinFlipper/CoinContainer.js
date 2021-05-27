66234892863817e6ac08c90f26787f1e4f272653
mounvip1525 <mounvip1525@gmail.com>
2021-05-27 11:18:37 +0530

    complete logic

--- react-mini-projects/src/components/CoinFlipper/CoinContainer.js
+++ react-mini-projects/src/components/CoinFlipper/CoinContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './styles.css'
 import Coin from './Coin';
+import { choice } from './helpers';
 
 export default class CoinContainer extends Component {
     static defaultProps = {
@@ -21,14 +22,43 @@ export default class CoinContainer extends Component {
             nHeads:0,
             nTails:0
         }
+        this.handleClick=this.handleClick.bind(this);
+    }
+    flipCoin(){
+        //returns a random side of the coin ex. {side:"heads",imgSrc:"https..."}
+        let newCoin = choice(this.props.coin);
+        // this.setState(st=>{
+        //     let newState = {
+        //         ...st,
+        //         curCoin:newCoin,
+        //         nFlips:st.nFlips+1
+        //     }
+        //     if(newCoin.side==="heads"){
+        //         newState.nHeads += 1;
+        //     } else {
+        //         newState.nTails += 1;
+        //     }
+        //     return newState
+        // });
+        this.setState(st=>{
+            return{
+                curCoin:newCoin,
+                nFlips:st.nFlips+1,
+                nHeads:st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+                nTails:st.nTails + (newCoin.side === "tails" ? 1 : 0),
+            };
+        })
+    }
+    handleClick(){
+        this.flipCoin();
     }
     render() {
         return (
             <div className="coin-container">
                 <h3>Lets Flip a Coin!</h3>
                 <Coin coin={this.state.curCoin} />
-                <button>Flip</button>
-                <p>Out of {this.state.nFlips} trials , there have been {this.state.nHeads} heads and {this.state.nHeads} tails.</p>
+                <button onClick={this.handleClick}>Flip</button>
+                <p>Out of {this.state.nFlips} trials , there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
             </div>
         )
     }
