5320cb0939796883445a1bcacbdcef1d7743ba0a
mounvip1525 <mounvip1525@gmail.com>
2021-06-07 20:24:07 +0530

    initial commit

---
+++ react-mini-projects/src/containers/CardsGenerator.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../components/CardsGenerator/styles.css'
+
+export default class CardsGenerator extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
