36c8ca5d16c04d86f414f4ee396ee89b550b0b76
mounvip1525 <mounvip1525@gmail.com>
2021-06-26 14:10:08 +0530

    soda

--- react-mini-projects/src/components/VendingMachine/Soda.js
+++ react-mini-projects/src/components/VendingMachine/Soda.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react'
-import './styles.css';
+import React, { Component } from "react";
+import Message from "./Message";
+import { Link } from "react-router-dom";
+import sodaImg from "../../assets/Soda.png";
+import "./styles.css";
 
-export default class Soda extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class Soda extends Component {
+  render() {
+    return (
+      <div className='Soda'>
+        <img src={sodaImg} alt='coca cola can' />
+        <Message>
+          <h1>SODAAAAA IS MY FAVORITE</h1>
+          <h3>But now I drink sparkling water instead</h3>
+          <p>(I still miss Soda)</p>
+          <Link to='/'>Go Back</Link>
+        </Message>
+        <img src={sodaImg} alt='coca cola can' />
+      </div>
+    );
+  }
 }
+
+export default Soda;
