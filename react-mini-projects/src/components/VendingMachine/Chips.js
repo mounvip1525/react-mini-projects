d56c5542755c4c18c20aa87bf82e1f131a34b151
mounvip1525 <mounvip1525@gmail.com>
2021-06-26 14:18:45 +0530

    chips

--- react-mini-projects/src/components/VendingMachine/Chips.js
+++ react-mini-projects/src/components/VendingMachine/Chips.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react'
-import './styles.css';
+import React, { Component } from "react";
+import Message from "./Message";
 
-export default class Chips extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+import { Link } from "react-router-dom";
+import chipsImg from "../../assets/Chips.png";
+
+import "./styles.css";
+
+class Chips extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bags: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const x = window.innerWidth * Math.random();
+    const y = window.innerHeight * Math.random();
+    this.setState(prevState => ({
+      bags: [...prevState.bags, { x, y }]
+    }));
+  }
+
+  render() {
+    const bags = this.state.bags.map((bag, i) => (
+      <img
+        key={i}
+        src={chipsImg}
+        className='bag'
+        style={{ top: `${bag.y}px`, left: `${bag.x}px` }}
+        alt="bag of lay's chips"
+      />
+    ));
+    return (
+      <div className='Chips'>
+        <Message>
+          <h1>bags eaten: {this.state.bags.length}</h1>
+          <button onClick={this.handleClick}>Gimme more chips</button>
+          <h1>
+            <Link to='/vendingMachine'>Go Back</Link>
+          </h1>
+        </Message>
+        {bags}
+      </div>
+    );
+  }
 }
+
+export default Chips;
