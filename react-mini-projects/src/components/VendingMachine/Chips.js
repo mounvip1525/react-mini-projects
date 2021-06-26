7b77d4fe2159cd635e0cdc5902383d56691b5af1
mounvip1525 <mounvip1525@gmail.com>
2021-06-26 13:00:28 +0530

    initial commit

---
+++ react-mini-projects/src/components/VendingMachine/Chips.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles.css';
+
+export default class Chips extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ react-mini-projects/src/components/VendingMachine/Soda.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './styles.css';
+
+export default class Soda extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
